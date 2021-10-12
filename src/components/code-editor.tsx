2f091492711e2cde6cd008d897c10abdf03b3e7b
dpak1999 <deepakkumardash313@gmail.com>
2021-10-12 21:12:26 +0530

    setting up options for editor

--- src/components/code-editor.tsx
+++ src/components/code-editor.tsx
@@ -1,9 +1,26 @@
 /** @format */
 
-import MonacoEditor from "@monaco-editor/react";
+import MonacoEditor from '@monaco-editor/react';
 
 const CodeEditor = () => {
-  return <MonacoEditor height="500px" language="javascript" theme="dark" />;
+  return (
+    <MonacoEditor
+      height="500px"
+      language="javascript"
+      theme="dark"
+      options={{
+        wordWrap: 'on',
+        matchBrackets: 'always',
+        minimap: { enabled: false },
+        showUnused: false,
+        folding: false,
+        lineNumbersMinChars: 3,
+        fontSize: 18,
+        scrollBeyondLastLine: false,
+        automaticLayout: true,
+      }}
+    />
+  );
 };
 
 export default CodeEditor;
