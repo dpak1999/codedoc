75327236ed62bd78e691475d96f3877cb7a68e93
dpak1999 <deepakkumardash313@gmail.com>
2021-12-11 21:35:02 +0530

    add : react md editor & conditionally toggling

--- src/index.tsx
+++ src/index.tsx
@@ -3,12 +3,13 @@
 import ReactDOM from 'react-dom';
 import './index.css';
 import 'bulmaswatch/superhero/bulmaswatch.min.css';
-import CodeCell from './components/CodeCell';
+// import CodeCell from './components/CodeCell';
+import TextEditor from './components/text-editor';
 
 ReactDOM.render(
   <div>
-    <CodeCell />
     {/* <CodeCell /> */}
+    <TextEditor />
   </div>,
   document.getElementById('root')
 );
