c2a7dcd3b516f92fcff6ba6db7a64c792117cb28
dpak1999 <deepakkumardash313@gmail.com>
2021-12-25 21:20:36 +0530

    add : action creators & wire redux to react app

--- src/index.tsx
+++ src/index.tsx
@@ -5,11 +5,13 @@ import './index.css';
 import 'bulmaswatch/superhero/bulmaswatch.min.css';
 // import CodeCell from './components/CodeCell';
 import TextEditor from './components/text-editor';
+import { Provider } from 'react-redux';
+import { store } from './state';
 
 ReactDOM.render(
-  <div>
+  <Provider store={store}>
     {/* <CodeCell /> */}
     <TextEditor />
-  </div>,
+  </Provider>,
   document.getElementById('root')
 );
