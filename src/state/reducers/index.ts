f55df1efa039c5ee32eeaf31611ee8264381ca93
dpak1999 <deepakkumardash313@gmail.com>
2021-12-25 20:47:43 +0530

    add : bundle & cell reduers

---
+++ src/state/reducers/index.ts
@@ -0,0 +1,12 @@
+/** @format */
+
+import { combineReducers } from 'redux';
+import cellsReducer from './cellsReducer';
+
+const reducers = combineReducers({
+  cells: cellsReducer,
+});
+
+export default reducers;
+
+export type RootState = ReturnType<typeof reducers>;
