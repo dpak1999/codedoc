5fb19e952e5bf309762784bb29401c46f15e70e4
dpak1999 <deepakkumardash313@gmail.com>
2021-12-26 19:18:36 +0530

    fix import & set initial state for cell reducer

--- src/index.tsx
+++ src/index.tsx
@@ -3,15 +3,13 @@
 import ReactDOM from 'react-dom';
 import './index.css';
 import 'bulmaswatch/superhero/bulmaswatch.min.css';
-// import CodeCell from './components/CodeCell';
-import TextEditor from './components/text-editor';
 import { Provider } from 'react-redux';
 import { store } from './state';
+import CellList from './components/cell-list';
 
 ReactDOM.render(
   <Provider store={store}>
-    {/* <CodeCell /> */}
-    <TextEditor />
+    <CellList />
   </Provider>,
   document.getElementById('root')
 );
