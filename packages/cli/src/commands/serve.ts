1e959cdc9377db603dea98b41289b6e229853f63
deepak <deepakkumardash313@gmail.com>
2022-01-18 23:49:47 +0530

    resolving directory paths

--- packages/cli/src/commands/serve.ts
+++ packages/cli/src/commands/serve.ts
@@ -1,5 +1,6 @@
 /** @format */
 
+import path from 'path';
 import { Command } from 'commander';
 import { serve } from 'local-api';
 
@@ -8,5 +9,6 @@ export const serveCommand = new Command()
   .description('Open a file for editing')
   .option('-p --port <number>', 'port to run server on', '4005')
   .action((filename = 'notebook.js', options: { port: string }) => {
-    serve(parseInt(options.port), filename, '/');
+    const dir = path.join(process.cwd(), path.dirname(filename));
+    serve(parseInt(options.port), path.basename(filename), dir);
   });
