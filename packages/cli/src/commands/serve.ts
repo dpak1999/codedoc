274e40882f96ff362318a6750a09ed0e775a27af
deepak <deepakkumardash313@gmail.com>
2022-01-20 13:08:42 +0530

    linking local client to local api

--- packages/cli/src/commands/serve.ts
+++ packages/cli/src/commands/serve.ts
@@ -4,6 +4,8 @@ import path from 'path';
 import { Command } from 'commander';
 import { serve } from 'local-api';
 
+const isProduction = process.env.NODE_ENV === 'production';
+
 export const serveCommand = new Command()
   .command('serve [filename]')
   .description('Open a file for editing')
@@ -11,7 +13,12 @@ export const serveCommand = new Command()
   .action(async (filename = 'notebook.js', options: { port: string }) => {
     try {
       const dir = path.join(process.cwd(), path.dirname(filename));
-      await serve(parseInt(options.port), path.basename(filename), dir);
+      await serve(
+        parseInt(options.port),
+        path.basename(filename),
+        dir,
+        !isProduction
+      );
       console.log(
         `Opened ${filename}. Navigate to http://localhost:${options.port} to edit the file.`
       );
--- packages/local-api/dist/index.js
+++ packages/local-api/dist/index.js
@@ -6,8 +6,21 @@ Object.defineProperty(exports, "__esModule", { value: true });
 exports.serve = void 0;
 /** @format */
 const express_1 = __importDefault(require("express"));
-const serve = (port, filename, dir) => {
+const http_proxy_middleware_1 = require("http-proxy-middleware");
+const path_1 = __importDefault(require("path"));
+const serve = (port, filename, dir, useProxy) => {
     const app = (0, express_1.default)();
+    if (useProxy) {
+        app.use((0, http_proxy_middleware_1.createProxyMiddleware)({
+            target: 'http://localhost:3000',
+            ws: true,
+            logLevel: 'silent',
+        }));
+    }
+    else {
+        const packagePath = require.resolve('local-client/build/index.html');
+        app.use(express_1.default.static(path_1.default.dirname(packagePath)));
+    }
     return new Promise((resolve, reject) => {
         app.listen(port, resolve).on('error', reject);
     });
