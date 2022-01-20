926e747b025afe3b17dcadf3bbc895b995f98e48
deepak <deepakkumardash313@gmail.com>
2022-01-20 13:22:29 +0530

    reading file content

--- packages/local-api/dist/routes/cells.js
+++ packages/local-api/dist/routes/cells.js
@@ -19,8 +19,23 @@ const promises_1 = __importDefault(require("fs/promises"));
 const path_1 = __importDefault(require("path"));
 const createCellsRouter = (filename, dir) => {
     const router = express_1.default.Router();
+    router.use(express_1.default.json());
     const fullPath = path_1.default.join(dir, filename);
-    router.get('/cells', (req, res) => __awaiter(void 0, void 0, void 0, function* () { }));
+    router.get('/cells', (req, res) => __awaiter(void 0, void 0, void 0, function* () {
+        try {
+            const result = yield promises_1.default.readFile(fullPath, { encoding: 'utf-8' });
+            res.send(JSON.parse(result));
+        }
+        catch (error) {
+            if (error.code === 'ENONT') {
+                yield promises_1.default.writeFile(fullPath, '[]', 'utf-8');
+                res.send([]);
+            }
+            else {
+                throw error;
+            }
+        }
+    }));
     router.post('/cells', (req, res) => __awaiter(void 0, void 0, void 0, function* () {
         const { cells } = req.body;
         yield promises_1.default.writeFile(fullPath, JSON.stringify(cells), 'utf-8');
--- packages/local-api/src/routes/cells.ts
+++ packages/local-api/src/routes/cells.ts
@@ -12,9 +12,24 @@ interface Cell {
 
 export const createCellsRouter = (filename: string, dir: string) => {
   const router = express.Router();
+  router.use(express.json());
+
   const fullPath = path.join(dir, filename);
 
-  router.get('/cells', async (req, res) => {});
+  router.get('/cells', async (req, res) => {
+    try {
+      const result = await fs.readFile(fullPath, { encoding: 'utf-8' });
+      res.send(JSON.parse(result));
+    } catch (error: any) {
+      if (error.code === 'ENONT') {
+        await fs.writeFile(fullPath, '[]', 'utf-8');
+        res.send([]);
+      } else {
+        throw error;
+      }
+    }
+  });
+
   router.post('/cells', async (req, res) => {
     const { cells }: { cells: Cell[] } = req.body;
     await fs.writeFile(fullPath, JSON.stringify(cells), 'utf-8');
